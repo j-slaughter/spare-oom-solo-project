216ecbbddff9a9060676ba73e208a9f0c9065ac9
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-04-04 14:35:55 -0700

    update webpack and styles for sass

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
 // Add styling for webpack to bundle
-import styles from './styles.css';
+import styles from './styles.scss';
 
 const root = createRoot(document.getElementById('root'));
 
