622e796fc357d5dce8f93551b642a8c2a7af5e9b
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-04-05 23:58:53 -0700

    update homepage reducer

--- client/store/homepageSlice.js
+++ client/store/homepageSlice.js
@@ -3,7 +3,6 @@ import { createSlice } from '@reduxjs/toolkit';
 // Initialize empty state for homepage
 const initialState = {
     url: {},
-    genres: {},
 };
 
 export const homepageSlice = createSlice({
@@ -14,14 +13,10 @@ export const homepageSlice = createSlice({
     getURL: (state, action) => {
         state.url = action.payload;
     },
-    // Update genres in state
-    getGenres: (state, action) => {
-        state.genres = action.payload;
-    },
   },
 });
 
 // Action creators generated for each case reducer function per docs
-export const { getURL, getGenres } = homepageSlice.actions;
+export const { getURL } = homepageSlice.actions;
 
 export default homepageSlice.reducer;
