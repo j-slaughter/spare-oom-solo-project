aa0c7011385e1a0c73d926d8ed355619ff24b5a4
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-04-05 18:09:02 -0700

    create footer

--- client/components/footer/Footer.jsx
+++ client/components/footer/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import ContentWrapper from '../contentWrapper/ContentWrapper.jsx';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import './footer.scss';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer'>
+            <ContentWrapper>
+                <ul className='menuItems'>
+                    <li className='menuItem'>Terms of Use</li>
+                    <li className='menuItem'>Privacy Policy</li>
+                </ul>
+                <div className='infoText'>
+                    The Spare Oom is not responsible for any dangerous quests, 
+                    witches' curses, firebreathing dragons, kidnapped loved ones, etc. encountered at 
+                    various destinations.
+                </div>
+                <div className='socialIcons'>
+                    <span className='icon'><FaFacebookF /></span>
+                    <span className='icon'><FaInstagram /></span>
+                    <span className='icon'><FaTwitter /></span>
+                </div>
+            </ContentWrapper>
+        </footer>
     );
 };
 
